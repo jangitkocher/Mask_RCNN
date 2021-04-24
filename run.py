ff7a0bc206c06900564ad7bd89719fdff3a2d268
jangitkocher <49987535+jangitkocher@users.noreply.github.com>
2021-04-24 08:50:40 +0200

    Added files

---
+++ run.py
@@ -0,0 +1,215 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+import mrcnn
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+# Import COCO config
+sys.path.append("samples/coco/")  # To find local version
+import coco
+
+# %matplotlib inline 
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = "images"
+
+"""## Configurations
+
+We'll be using a model trained on the MS-COCO dataset. The configurations of this model are in the ```CocoConfig``` class in ```coco.py```.
+
+For inferencing, modify the configurations a bit to fit the task. To do so, sub-class the ```CocoConfig``` class and override the attributes you need to change.
+"""
+
+import xml.etree.ElementTree as ET
+
+class SteelbarDataset(utils.Dataset):
+    def load_dataset(self, dataset_dir):
+        self.add_class('dataset', 1, 'Steelbars')
+        
+        # find all images
+        for i, filename in enumerate(os.listdir(dataset_dir)):
+            if '.jpg' in filename:
+                self.add_image('dataset', 
+                               image_id=i, 
+                               path=os.path.join(dataset_dir, filename), 
+                               annotation=os.path.join(dataset_dir, filename.replace('.jpg', '.xml')))
+    
+    # extract bounding boxes from an annotation file
+    def extract_boxes(self, filename):
+        # load and parse the file
+        tree = ET.parse(filename)
+        # get the root of the document
+        root = tree.getroot()
+        # extract each bounding box
+        boxes = []
+        classes = []
+        for member in root.findall('object'):
+            xmin = int(member[4][0].text)
+            ymin = int(member[4][1].text)
+            xmax = int(member[4][2].text)
+            ymax = int(member[4][3].text)
+            boxes.append([xmin, ymin, xmax, ymax])
+            classes.append(self.class_names.index(member[0].text))
+        # extract image dimensions
+        width = int(root.find('size')[0].text)
+        height = int(root.find('size')[1].text)
+        return boxes, classes, width, height
+ 
+    # load the masks for an image
+    def load_mask(self, image_id):
+        # get details of image
+        info = self.image_info[image_id]
+        # define box file location
+        path = info['annotation']
+        # load XML
+        boxes, classes, w, h = self.extract_boxes(path)
+        # create one array for all masks, each on a different channel
+        masks = np.zeros([h, w, len(boxes)], dtype='uint8')
+        # create masks
+        for i in range(len(boxes)):
+            box = boxes[i]
+            row_s, row_e = box[1], box[3]
+            col_s, col_e = box[0], box[2]
+            masks[row_s:row_e, col_s:col_e, i] = 1
+        return masks, np.asarray(classes, dtype='int32')
+    
+    def image_reference(self, image_id):
+        info = self.image_info[image_id]
+        return info['path']
+
+from mrcnn.config import Config
+
+class SteelbarConfig(Config):
+    # Give the configuration a recognizable name
+    NAME = "steelbar_detection"
+    
+    NUM_CLASSES = 1 + 4
+
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = SteelbarConfig()
+config.display()
+
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+"""## Create Model and Load Trained Weights"""
+
+# Create training and validation set
+# train set
+import os
+
+a = os.getcwd()
+print(a)
+dataset_train = SteelbarDataset()
+dataset_train.load_dataset('./images')
+dataset_train.prepare()
+print('Train: %d' % len(dataset_train.image_ids))
+ 
+# test/val set
+dataset_val = SteelbarDataset()
+dataset_val.load_dataset('./images')
+dataset_val.prepare()
+print('Test: %d' % len(dataset_val.image_ids))
+
+# Load and display random samples
+image_ids = np.random.choice(dataset_train.image_ids, 4)
+for image_id in image_ids:
+    image = dataset_train.load_image(image_id)
+    mask, class_ids = dataset_train.load_mask(image_id)
+    visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
+
+# Create model object in inference mode.
+# model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+# model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+# --------------------------------------------------------
+
+# Create model in training mode
+model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir="./Mask_RCNN")
+
+# Which weights to start with?
+init_with = "coco"  # imagenet, coco, or last
+
+if init_with == "imagenet":
+    model.load_weights(model.get_imagenet_weights(), by_name=True)
+elif init_with == "coco":
+    # Load weights trained on MS COCO, but skip layers that
+    # are different due to the different number of classes
+    # See README for instructions to download the COCO weights
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])
+elif init_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last(), by_name=True)
+
+# Train the head branches
+# Passing layers="heads" freezes all layers except the head
+# layers. You can also pass a regular expression to select
+# which layers to train by name pattern.
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE, 
+            epochs=5, 
+            layers='heads')
+
+# Fine tune all layers
+# Passing layers="all" trains all layers. You can also 
+# pass a regular expression to select which layers to
+# train by name pattern.
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE / 10,
+            epochs=10, 
+            layers="all")
+
+"""## Run Object Detection"""
+
+# Load a random image from the images folder
+file_names = next(os.walk(IMAGE_DIR))[2]
+image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+# Run detection
+results = model.detect([image], verbose=1)
+
+# Visualize results
+r = results[0]
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                            class_names, r['scores'])
